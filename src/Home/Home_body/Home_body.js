4ca3e920ed43ec40ec6840d950c1933e82b349c5
anudevpm007 <anudevpm007@gmail.com>
2024-06-29 19:41:46 +0530

    first

--- src/Home/Home_body/Home_body.js
+++ src/Home/Home_body/Home_body.js
@@ -3,21 +3,28 @@ import "./Home_body.css";
 import services_data from "../../Data/Services_data";
 import Services from "./services/services";
 
- class Home_body extends Component {
+class Home_body extends Component {
   render() {
     return (
       <div className="Home_body">
-        
         <div className="container">
-          <h1>Services</h1>
-        <div className="row">
-          {
-            services_data.map((data)=>
-              <Services id = {data.id} heading = {data.Heading} pera = {data.Pera}/>
-            )
-          }
+          <div className="row">
+            <div className="col-12" style={{marginTop:40, marginBottom:50,fontWeight:"bold",fontSize:18}}>
+            Only AMD Ryzen™ processors feature models with exclusive AMD 3D V-Cache™ technology for a massive gaming performance boost.4 AMD combines its top-end Ryzen 7000X3D series processors, with up to a colossal 144MB of on-chip memory, paired with the most advanced processor cores you can get in a gaming PC so enthusiasts can harness the power of the ultimate gaming and creator performance in one chip.5 No workload is off limits with AMD Ryzen processors.
+            </div>
           </div>
-      </div>
+          <h1>Services</h1>
+          <div className="row">
+            {services_data.map((data) => (
+              <Services id={data.id} heading={data.Heading} pera={data.Pera} />
+            ))}
+          </div>
+          <div className="row">
+            <div className="col-12" style={{marginTop:40, marginBottom:50,fontWeight:"bold",fontSize:18}}>
+            The Internet of things describes devices with sensors, processing ability, software and other technologies that connect and exchange data with other devices and systems over the Internet or other communications networks. The Internet of things encompasses electronics, communication, and computer science engineering
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Home/Home_body/services/services.js
+++ src/Home/Home_body/services/services.js
@@ -15,21 +15,24 @@ export default class Services extends Component {
     };
     return (
       <div className="col-lg-6">
-        <Link to={"/StartUp/services/"} className="S_link" style={{ textDecoration: "none" }}>
-          <Card
-          className="S_card"
-            style={{
-              padding: 10,
-              margin: 30,
-
-              boxShadow: "black 2px 2px 5px",
-            }}
-          >
-            <Card.Title>{this.state.Heading}</Card.Title>
-            <Card className="I_S_card" style={{ padding: 10, marginTop: 10 }}>
-              <CardText id="ser_link">{this.state.Pera}</CardText>
+        <Link
+          to={"/StartUp/services/"}
+          className="S_link"
+          style={{ textDecoration: "none" }}
+        >
+          <div id="ser">
+            <Card
+              className="S_card"
+              style={{
+                padding: 10,
+              }}
+            >
+              <Card.Title>{this.state.Heading}</Card.Title>
+              <Card className="I_S_card" style={{ padding: 10, marginTop: 10 }}>
+                <CardText id="ser_link">{this.state.Pera}</CardText>
+              </Card>
             </Card>
-          </Card>
+          </div>
         </Link>
       </div>
     );
